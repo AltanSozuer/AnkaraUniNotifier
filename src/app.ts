b7340bd5de37db4373b27ea3f23d00c60143f363
Altan SOZUER <sozueraltan@gmail.com>
2023-05-21 00:28:46 +0300

    cors is integrated to app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express from 'express'
+import cors from 'cors';
 const app = express();
 
 import { configureEnvFile } from './utils/EnvConfig';
 configureEnvFile()
 
 app.use(express.json())
-
+app.use(cors({
+    origin: 'http://localhost:19006'
+}))
 import configureAllRoutes from './routes/index';
 configureAllRoutes(app);
 
