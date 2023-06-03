a01c7e89f868972258cb5bce82857ab1e360fd1e
Altan SOZUER <sozueraltan@gmail.com>
2023-06-03 18:03:29 +0300

    /register, /refreshToken and /login routes are updated and also /logout endpoint is added to Auth.routes

--- src/routes/Auth.routes.ts
+++ src/routes/Auth.routes.ts
@@ -3,8 +3,7 @@ import { body, Result, validationResult } from 'express-validator';
 import UserService from '../services/UserService';
 import { isEmailValid } from '../utils/RegexHelpers';
 import loggerFunc from '../utils/Logger';
-import { generateAccessToken, getRefreshToken, isPasswordEqualToItsHashedVersion } from '../utils/AuthHelpers';
-import jwtDecode from 'jwt-decode';
+import { hashPassword, isPasswordEqualToItsHashedVersion } from '../utils/AuthHelpers';
 import TokenService from '../services/TokenService';
 const router = express.Router();
 const logger = loggerFunc(__filename);
@@ -41,21 +40,17 @@ router.post('/register',
                 const { name, surname, email, password }  = req.body;
                 const userService = new UserService();
                 logger.debug('POST /register given parameters: ', { name, surname, email, password })
-
+                const hashedPassword = await hashPassword(password);
                 const checkUserAlreadyRegistered = await userService.fetchOneWithoutPassword(email);
                 if(checkUserAlreadyRegistered){
                     const errDetailObj = { msg: 'User is already exist who has given email', email };
                     logger.debug('POST /register is failed parameters: ', errDetailObj)
                     return res.status(400).json({ error: errDetailObj })
                 } 
-                const createdUser = await userService.create({ name, surname, email, password })
-                const accessToken = generateAccessToken(createdUser.email)
-                const decodedAccessToken: {exp: string} = jwtDecode(accessToken);
-                const expireAt = decodedAccessToken.exp;
+                const createdUser = await userService.create({ name, surname, email, password: hashedPassword })
+                
                 res.status(201).json({
                     msg: 'User registered successfully',
-                    accessToken,
-                    expireAt, 
                     data: createdUser 
                 })
             }
@@ -97,26 +92,24 @@ router.post('/login',
                 const { email, password } = req.body;
                 const userInDb = await new UserService().fetchOneWithPassword(email);
                 if(!userInDb) {
-                    return res.status(401).json({ msg: 'User does not exist' })
+                    return res.status(401).json({ msg: 'Invalid email or password' })
                 }
                 const isPassValid = await isPasswordEqualToItsHashedVersion(password, userInDb.password);
                 if(!isPassValid) {
-                    return res.status(401).json({ msg: 'Password is invalid' })
+                    logger.debug('POST /login | Given password is not valid:  ', { isPassValid })
+                    return res.status(401).json({ msg: 'Invalid email or password' })
                 }
 
-                const accessToken = generateAccessToken(userInDb.email)
-                const decodedAccessToken: {exp: string} = jwtDecode(accessToken);
-                const expireAt = decodedAccessToken.exp;
-                const refreshToken = getRefreshToken();
-                await new TokenService().createToken({refreshToken, user: userInDb._id})
+                const { accessToken, refreshToken } = await new TokenService().generateTokens(userInDb._id, userInDb.email);
+        
                 res.status(200).json({
                     msg: 'Successfully logged in',
                     accessToken,
                     refreshToken,
-                    expireAt
                 })
             }
             catch(err) {
+                logger.debug('POST /login is failed: ', { result: err })
                 res.status(500).json({ 
                     error: {
                         msg: `/login is failed.`,
@@ -143,19 +136,24 @@ router.post('/auth/refreshToken',
         })
         if(validationRes.isEmpty()) {
             try {
+                const tokenService = new TokenService();
                 const { refreshToken } = req.body;
-                const userRelatedTokenInDb = await new TokenService().fetchOneByRefreshToken(refreshToken);
-                if(!userRelatedTokenInDb) {
+                const resultOfVerification = await tokenService.verifyRefreshToken(refreshToken);
+                if(!resultOfVerification) {
+                    logger.debug('POST /auth/refreshToken | Refresh token verification is failed: ', { resultOfVerification })
                     return res.status(401).json({ message: 'Invalid refresh token' })
                 }
-                const userInDb = await new UserService().fetchOneWithoutPassword(userRelatedTokenInDb._id);
+                
+                const userInDb = await new UserService().fetchOneWithoutPassword(resultOfVerification.email);
                 if(!userInDb) {
+                    logger.debug('POST /auth/refreshToken | User could not be found: ', { userInDb })
                     return res.status(401).json({ message: 'Invalid refresh token' })
                 }
-                const token = generateAccessToken(userInDb.email);
-                return res.status(201).json({accessToken: token})
+                const { accessToken } = await tokenService.generateTokens(userInDb._id ,userInDb.email);
+                return res.status(201).json({accessToken})
             }
             catch(err) {
+                logger.debug('POST /auth/refreshToken is failed: ', { error: err })
                 res.status(500).json({ 
                     error: {
                         msg: `Could not refresh token.`,
@@ -166,6 +164,41 @@ router.post('/auth/refreshToken',
         }
         else {
             logger.error('POST /auth/refreshToken | Parameters are not valid : ', {
+                result: validationRes.array()
+            })
+            res.status(400).json({ error: validationRes.array() })
+        }
+})
+
+
+router.delete('/logout',
+    body('refreshToken').exists().withMessage('refreshToken must be exist').bail().escape(),
+    async (req: Request, res: Response) => {
+        logger.info('DELETE /auth/refreshToken is called')
+        const validationRes: Result = validationResult(req);
+        logger.debug('DELETE /auth/refreshToken parameter validation result: ', {
+            result: validationRes
+        })
+        if(validationRes.isEmpty()) {
+            try {
+                const tokenService = new TokenService();
+                const { refreshToken } = req.body;
+                await tokenService.deleteTokenByRefreshToken(refreshToken);
+                logger.debug('DELETE /auth/refreshToken | Refresh token is not exist in db so logged out: ')
+                return res.status(200).json({ message: 'Successfully logged out.' });
+            }
+            catch(err) {
+                logger.debug('DELETE /auth/refreshToken is failed: ', { error: err })
+                res.status(500).json({ 
+                    error: {
+                        msg: `Could not refresh token.`,
+                        detail: err 
+                    }
+                })
+            }
+        }
+        else {
+            logger.error('DELETE /logout | Parameters are not valid : ', {
                 result: validationRes.array()
             })
             res.status(400).json({ error: validationRes.array() })
